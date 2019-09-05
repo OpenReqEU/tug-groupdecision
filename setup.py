bfd6def80a7b6c621e884faed371b5f2f08f6932
Ralph Samer <ralph.samer@student.tugraz.at>
2019-09-05 12:22:35 +0200

    Updated readme

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ REQUIRES = ["connexion"]
 setup(
     name=NAME,
     version=VERSION,
-    description="Twitter Social Popularity Service",
+    description="Group Decision Service",
     author_email="rsamer@ist.tugraz.at",
     url="",
-    keywords=["Swagger", "Twitter Social Popularity Service"],
+    keywords=["Swagger", "Group Decision Service"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={'': ['swagger/swagger.yaml']},
@@ -29,7 +29,9 @@ setup(
     entry_points={
         'console_scripts': ['application=application.__main__:main']},
     long_description="""\
-    Twitter Social Popularity Service
+    This group-based recommendation service accepts and analyzes a set of requirement ratings and individual stakeholder preferences within a requirements project.
+    For every requirement, the service detects (rating) conflicts (i.e., conflicts of opinions and rating dissents) and computes a utility value reflecting the appropriateness of the stakeholders for the requirement by considering the relevance of the requirements for the individual stakeholders as well as the availability of these stakeholders.
+    The stakeholders with the highest utility values are recommended as potential candidates to be assigned to the requirements.
     """
 )
 
